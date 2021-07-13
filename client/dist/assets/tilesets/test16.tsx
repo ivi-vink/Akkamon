187c4eab452ac2c44a1417e5a08d4ab99c560f5a
Mike Vink <mike1994vink@gmail.com>
2021-07-13 10:02:41 +0200

    feat(loading tilemap): tilemap created in tiled

---
+++ client/dist/assets/tilesets/test16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="test16" tilewidth="16" tileheight="16" tilecount="44" columns="11">
+ <image source="super-mario-tiles.png" width="176" height="64"/>
+</tileset>
--- client/src/game.ts
+++ client/src/game.ts
@@ -9,15 +9,23 @@ export default class Demo extends Phaser.Scene
 
     preload ()
     {
-        this.load.image("tiles", "assets/catastrophi_tiles_16_blue.png");
-        this.load.tilemapCSV("map", "assets/catastrophi_level2.csv");
+        this.load.image("tiles", "assets/tilesets/tuxmon-sample-32px-extruded.png");
+        // load from json!
+        this.load.tilemapTiledJSON("map", "assets/tilemaps/tuxemon-town.json");
     }
 
     create ()
     {
-        const map = this.make.tilemap({ key: "map", tileWidth: 16, tileHeight: 16 });
-        const tileset = map.addTilesetImage("tiles");
-        const layer = map.createLayer(0, tileset, 0, 0); // layer index, tileset, x, y
+        const map = this.make.tilemap({ key: "map" });
+
+        // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
+        // Phaser's cache (i.e. the name you used in preload)
+        const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
+
+        // Parameters: layer name (or index) from Tiled, tileset, x, y
+        const belowLayer = map.createLayer("Below Player", tileset, 0, 0);
+        const worldLayer = map.createLayer("World", tileset, 0, 0);
+        const aboveLayer = map.createLayer("Above Player", tileset, 0, 0);
     }
 }
 
