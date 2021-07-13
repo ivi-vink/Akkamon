c43fe73a617d20192385ed169cd0215898dad66c
Mike Vink <mike1994vink@gmail.com>
2021-07-13 11:24:10 +0200

    feat(Scene): make the Scene object functional

--- client/src/game.ts
+++ client/src/game.ts
@@ -1,24 +1,9 @@
 import 'phaser';
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
-    }
-
-    preload ()
-    {
-        this.load.image("tiles", "assets/catastrophi_tiles_16_blue.png");
-        this.load.tilemapCSV("map", "assets/catastrophi_level2.csv");
-    }
-
-    create ()
-    {
-        const map = this.make.tilemap({ key: "map", tileWidth: 16, tileHeight: 16 });
-        const tileset = map.addTilesetImage("tiles");
-        const layer = map.createLayer(0, tileset, 0, 0); // layer index, tileset, x, y
-    }
+interface AkkamonScene extends Phaser.Scene {
+    preload: () => void
+    create: () => void
+    update: (time: Number, delta: Number) => void
 }
 
 const config = {
@@ -26,7 +11,28 @@ const config = {
     backgroundColor: '#125555',
     width: 800,
     height: 600,
-    scene: Demo
+    pixelArt: true,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
 };
 
 const game = new Phaser.Game(config);
+
+
+function preload ()
+{
+
+}
+
+function create ()
+{
+
+}
+
+function update(time: Number, delta: Number)
+{
+
+}
